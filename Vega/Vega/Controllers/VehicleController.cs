7a898c42eaf7bb41e42bfc1fb11915f4f4da7c98
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2020-08-29 13:22:32 +0300

    Remove vehicle

--- Vega/Vega/Controllers/VehicleController.cs
+++ Vega/Vega/Controllers/VehicleController.cs
@@ -49,6 +49,10 @@ namespace Vega.Controllers
 
             var vehicle = await _context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
 
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
             _mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
 
             vehicle.LastUpdate = DateTime.UtcNow;
@@ -58,5 +62,22 @@ namespace Vega.Controllers
             return Ok(result);
 
         }
-    }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await _context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(vehicle);
+            await _context.SaveChangesAsync();
+
+            return Ok(id);
+        }
+
+        }
 }
